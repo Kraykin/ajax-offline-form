379945e5fd3a1f9900076d7f39248a957d1845c9
Sergey Kraykin <kraykin@gmail.com>
2016-03-26 16:50:17 +0100

    Add JS to app, urls listing to view, actions to controller

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  var url_list = [];
+  $('form.url_form').submit(function(){
+    var url = $("#website_url").val();
+    if (/^(http|https|ftp):\/\//i.test(url))
+      domain = url.split('//')[1];
+    else
+      domain = url;
+    if (/^(([a-z0-9][a-z0-9_-]+\.)+([a-z]{2,}))(:(\d+))?\/?/i.test(domain)) {
+      display_url = domain.replace("www.","").split('/')[0];
+      if($.inArray(display_url, collection_urls) < 0){
+        $("tbody").prepend("<tr><td>"+ display_url +"</td></tr>");
+        url_list.push(display_url);
+        $("#website_url").val("");
+        collection_urls.push(display_url);
+      }
+    }
+    return false;
+  });
+
+  setTimeout(function() {worker();}, 15000 );
+  function worker(){
+    if(url_list.length > 0){
+      $.ajax({
+        url: "/website",
+        type: "post",
+        data: {url_list: url_list},
+        success: function(response, textStatus, jqXHR){
+          $.each(response, function(index, value) { 
+            url_list.pop(value); 
+          });
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+        },
+        complete: function(){
+        }
+      });
+    }
+    $.ajax({
+      url: "/website/online",
+      type: "get",
+      success: function(response, textStatus, jqXHR){
+        $.each(response, function(index, value) {
+    		  if($.inArray(value, collection_urls) == -1){
+    			  collection_urls.push(value);
+    			  $("tbody").prepend("<tr><td>"+ value +"</td></tr>");
+    		  }
+        });
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+      },
+      complete: function(){
+      }
+    });
+    setTimeout(function() {worker();}, 15000 );
+  }
+});
--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,10 +1,16 @@
 class WebsiteController < ApplicationController
   def new
+    @websites = Website.order 'created_at DESC'
   end
 
   def create
+    params[:url_list].each do |url|
+      Website.new(:url => url).save
+    end
+    render :json=>params[:url_list].to_json
   end
 
   def online
+    render :json=>Website.all.map(&:url).to_json
   end
 end
